5f6c0ab0947aa2669806e2d512a6ae604a07971d
matsutomu <matsu.tom@gmail.com>
2018-05-05 12:40:23 +0900

    dynamic programming

---
+++ src/main/scala/Alds/Alds111B.scala
@@ -0,0 +1,35 @@
+package Alds
+
+import scala.io.StdIn
+
+object Alds111B extends App {
+
+  private val n = StdIn.readLine().toInt
+  val memo = Array.fill(n+1, n+1)(0)
+  val p = Array.fill(n+1)(0)
+  
+  (0 until n).foreach{ i =>
+    val Array(r, c) = StdIn.readLine().split(' ').map(_.toInt)
+    p(i) = r 
+    p(i+1) = c
+  }
+  //println(p.toList)
+  
+  def matrixChainMultiplication(): Unit = {
+    (2 to n).foreach{ l =>
+      (1 to (n - l + 1)).foreach{ i =>
+        val j = i + l - 1
+        memo(i)(j) = Int.MaxValue
+        (i until j).foreach{ k =>
+          memo(i)(j) = scala.math.min(memo(i)(j), memo(i)(k) + memo(k+1)(j) + p(i-1) * p(k) * p(j))
+        }
+      }
+    }
+  }
+  
+  matrixChainMultiplication()
+
+  println(memo(1)(n))
+  
+}
+
---
+++ src/main/scala/Alds/Alds111C.scala
@@ -0,0 +1,56 @@
+package Alds
+
+import scala.io.StdIn
+
+object Alds111C extends App {
+
+  private val N = 1000
+
+  def lcs(x: String, y: String): Int = {
+    val a = Array.fill(N+1, N+1)(0)
+    val m = x.length
+    val n = y.length
+    var maxl = 0
+    var tempX = " " + x
+    var tempY = " " + y
+
+    (1 to m).foreach{ i =>
+      (1 to n).foreach{ j =>
+        if(tempX(i) == tempY(j)){
+          a(i)(j) = a(i-1)(j-1) + 1
+        } else {
+          a(i)(j) = scala.math.max(a(i-1)(j), a(i)(j-1))
+        }
+        maxl = scala.math.max(maxl, a(i)(j))
+      }
+    }
+    maxl
+  }
+
+  val cnt = StdIn.readLine().toInt
+  val result = (0 until cnt).map{ _ =>
+    lcs(StdIn.readLine(), StdIn.readLine())
+  }
+
+  result.foreach(println)
+}
+
+/*
+  def fib(k: Int): Int = {
+    if(a(k) != 0){
+      a(k)
+    } else {
+      a(k) = fib(k-1) + fib(k-2)
+      a(k)
+    }
+  }
+  
+  // too late
+  def fib(k: Int): Int = {
+    if(k == 0 || k == 1) {
+      1
+    } else {
+      fib(k-1) + fib(k-2)
+    }
+  }
+ */
