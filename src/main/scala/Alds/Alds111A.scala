16b192ddd1bb56e7b61f4094802e84f19bbf7959
matsutomu <matsu.tom@gmail.com>
2018-05-06 07:15:15 +0900

    graph

--- src/main/scala/Alds/Alds111A.scala
+++ src/main/scala/Alds/Alds111A.scala
@@ -3,15 +3,15 @@ package Alds
 import scala.io.StdIn
 
 object Alds111A extends App {
-  
+
   val n = StdIn.readLine().toInt
   val result = Array.ofDim[Int](n, n)
   val r = (0 until n).map { _ =>
     val a = StdIn.readLine().split(' ').map(_.toInt)
     (0 until a(1)).foreach { k =>
-      result(a(0)-1)(a(2+k)-1) = 1
+      result(a(0) - 1)(a(2 + k) - 1) = 1
     }
-    result(a(0)-1).mkString(" ")
+    result(a(0) - 1).mkString(" ")
   }
 
   println(r.mkString(f"%n"))
